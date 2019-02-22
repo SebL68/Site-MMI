a24a1d8b51ff8e3312baa656fb0a43b0182dd031
SebL68 <31206445+SebL68@users.noreply.github.com>
2019-02-22 03:09:52 +0100

    News

---
+++ news.php
@@ -0,0 +1,16 @@
+<style>
+	main{
+		display: flex;
+	}
+	.fb-page{
+		margin: auto;
+	}
+
+</style>
+<div id="fb-root"></div>
+<script>
+	document.querySelector(".fb-page").dataset.height = document.querySelector("main").offsetHeight + "px";
+</script>
+<script  src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.2"></script>
+
+<div class="fb-page" data-width="500px" data-height="" data-href="https://www.facebook.com/srcmulhouse/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/srcmulhouse/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/srcmulhouse/">Département MMI - IUT de Mulhouse</a></blockquote></div>
